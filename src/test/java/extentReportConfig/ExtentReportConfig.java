ae6f1b16ee70e33531d4076e15776188cf5e1c83
Karan Salve <127831639+PlasmaPropulsionEngine@users.noreply.github.com>
2024-12-19 17:04:19 +0530

    Update ExtentReportConfig.java
    cdn

--- src/test/java/extentReportConfig/ExtentReportConfig.java
+++ src/test/java/extentReportConfig/ExtentReportConfig.java
@@ -15,7 +15,9 @@ public static ExtentReports getExtentreportObject()
 	String reportpath = System.getProperty("user.dir")+"//reports//index.html";
 	
 	ExtentSparkReporter sparkReporter=new ExtentSparkReporter(reportpath);
-	
+
+	sparkReporter.config().setResourceCDN("<link rel=\"apple-touch-icon\" href=\"https://cdn.jsdelivr.net/gh/extent-framework/extent-github-cdn@b00a2d0486596e73dd7326beacf352c639623a0e/commons/img/logo.png\">\r\n"
+			+ "");
 	sparkReporter.config().setDocumentTitle("TEST RESULTS");
 	sparkReporter.config().setReportName("Rest Api Automation Tests");
 	sparkReporter.config().setTheme(Theme.DARK);	
